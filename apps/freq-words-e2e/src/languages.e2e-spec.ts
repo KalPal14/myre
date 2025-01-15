0663d0a86b58ea16d5add31cf00bfc36f7b89d11
KalPal <reznichenkomykola1@gmail.com>
2025-01-15 11:42:55 +0200

    test: add e2e tests for languages

---
+++ apps/freq-words-e2e/src/languages.e2e-spec.ts
@@ -0,0 +1,76 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+
+import { LANGUAGES_URLS } from '~libs/routes/freq-words';
+
+import { AppModule } from '~/freq-words/app.module';
+import {
+	ENGLISH_LANGUAGE_ENTITY,
+	URKAINIAN_LANGUAGE_ENTITY,
+} from '~/freq-words/resources/languages/mocks/languages';
+
+describe('AppController (e2e)', () => {
+	let app: INestApplication;
+
+	beforeEach(async () => {
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			imports: [AppModule],
+		}).compile();
+
+		app = moduleFixture.createNestApplication();
+		await app.init();
+	});
+
+	afterAll(async () => {
+		await app.close();
+	});
+
+	describe('get many', () => {
+		describe(`do not pass parameter q`, () => {
+			it('should return all languages', async () => {
+				const resp = await request(app.getHttpServer()).get(LANGUAGES_URLS.getMany);
+
+				expect(resp.status).toBe(200);
+				expect(Array.isArray(resp.body)).toBe(true);
+				expect(resp.body.length).toBeGreaterThan(1);
+				expect(resp.body).toEqual(
+					expect.arrayContaining([ENGLISH_LANGUAGE_ENTITY, URKAINIAN_LANGUAGE_ENTITY])
+				);
+			});
+		});
+
+		describe(`pass parameter q`, () => {
+			it('should return filtered languages based on query', async () => {
+				const resp = await request(app.getHttpServer())
+					.get(LANGUAGES_URLS.getMany)
+					.query({ q: 'Eng' });
+
+				expect(resp.status).toBe(200);
+				expect(Array.isArray(resp.body)).toBe(true);
+				expect(resp.body).toEqual(expect.arrayContaining([ENGLISH_LANGUAGE_ENTITY]));
+				expect(resp.body).not.toEqual(expect.arrayContaining([URKAINIAN_LANGUAGE_ENTITY]));
+			});
+		});
+	});
+
+	describe('get one', () => {
+		describe(`pass the id of an existing language`, () => {
+			it('should return a language by id', async () => {
+				const resp = await request(app.getHttpServer()).get(LANGUAGES_URLS.get(1));
+
+				expect(resp.status).toBe(200);
+				expect(resp.body).toEqual(ENGLISH_LANGUAGE_ENTITY);
+			});
+		});
+
+		describe(`pass the id of a non-existing language`, () => {
+			it('should return err', async () => {
+				const resp = await request(app.getHttpServer()).get(LANGUAGES_URLS.get(999));
+
+				expect(resp.status).toBe(404);
+				expect(resp.body).toEqual({ err: `Language #999 not found` });
+			});
+		});
+	});
+});
