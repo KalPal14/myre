4f1b5db15de3612012641f6633069f785d9356a1
KalPal <reznichenkomykola1@gmail.com>
2025-02-18 12:27:08 +0200

    fix: problem with not running CS in firefox

--- apps/highlight-extension-fe/src/content-scripts/highlights/index.tsx
+++ apps/highlight-extension-fe/src/content-scripts/highlights/index.tsx
@@ -5,9 +5,14 @@ import ReactShadowRoot from 'react-shadow-root';
 import styles from './highlights.shadow-dom.scss';
 import Highlights from './highlights';
 
-window.onload = (): void => init();
+init();
 
 function init(): void {
+	if (document.readyState === 'loading') {
+		document.addEventListener('DOMContentLoaded', init);
+		return;
+	}
+
 	const highlightsMarker = document.createElement('highlights-ext-container');
 	highlightsMarker.id = 'highlights-ext-container';
 	document.body.append(highlightsMarker);
